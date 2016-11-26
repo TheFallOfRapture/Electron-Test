1184135b0a41b05723069fd24ecece4309a016f8
Fernando Gonzalez <fallofrapture59@gmail.com>
2016-11-26 17:46:38 -0500

    Added link.js, sidebar.js, index.js using Electron features

--- app/js/index.js
+++ app/js/index.js
@@ -1,24 +1,27 @@
+const BrowserWindow = require("electron").remote.BrowserWindow
+const path = require("path")
+
+document.getElementById("newWindowButton").addEventListener("click", (event) => {
+    let win = new BrowserWindow({width: 1024, height: 768})
+    win.on("close", () => win = null)
+    win.loadURL(`file://${__dirname}/../html/other.html`)
+    win.show()
+})
+
 function buttonClick() {
     alert("Have some blue text!")
     var paragraphs = Array.prototype.slice.call(document.getElementsByTagName('p'))
     paragraphs.forEach(el => el.style.color = "#2196F3")
+    document.getElementById('firstP').style.cursor = "pointer"
 }
 
 function changeTitleColor() {
-    document.getElementById('titleText').innerHTML = "Psst! Click the first paragraph!"
+    document.getElementById('titleText').innerHTML = "Click the first paragraph!"
+    document.getElementById('firstP').style.cursor = "pointer"
 }
 
 function changeToRed(id) {
     id.style.color = "red"
     document.getElementById('firstButton').style.color = "#2196F3"
-}
-
-function toggleSubmenu(id) {
-    var children = Array.prototype.slice.call(id.children).filter(el => el.tagName === "DIV")
-    var expanded = children[0].style.display === "block"
-    if (expanded) {
-        children.forEach(el => el.style.display = "none")
-    } else {
-        children.forEach(el => el.style.display = "block")
-    }
+    document.getElementById('firstP').style.cursor = "auto"
 }
